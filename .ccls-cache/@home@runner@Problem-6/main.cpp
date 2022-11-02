90c8bf89ee60452d74b2a463d5907882df6583bd
Edwin Kurjak <112413421+elkurjak@users.noreply.github.com>
2022-11-02 06:51:54 +0000

    Initialized variables and started a for loop.

--- .ccls-cache/@home@runner@Problem-6/main.cpp
+++ .ccls-cache/@home@runner@Problem-6/main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
 
+using namespace std;
+
+struct car{
+  int num;
+  int mil;
+  int gall;
+  };
+
 int main() {
-  std::cout << "Hello World!\n";
+  const int Carnum = 5;
+  car cars[Carnum];
+  for (int i=0; i<Carnum; i++){
+    cin >> cars[i].num;
+    cin >> cars[i].mil;
+    cin >> cars[i].gall;
+  }
+  double MPGtotal = 0;
+   for (int i = 0; i<Carnum; i++){
+     double mpg = cars[i].mil / cars[i].gall;
+     MPGtotal += mpg;
+     cout<< "Car Number " 
+   }
+  
 }
--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 
+using namespace std;
+
+struct car{
+  int num;
+  int mil;
+  int gall;
+  };
+
 int main() {
-  std::cout << "Hello World!\n";
-}
+  const int Carnum = 5;
+  car cars[Carnum];
+  for (int i=0; i<Carnum; i++){
+    cin >> cars[i].num;
+    cin >> cars[i].mil;
+    cin >> cars[i].gall;
+  }
+ 
